f6dd5af8234957720be86b7736b771eb863da288
ADLKZZZ <ravurinarayana0123@gmail.com>
2024-12-19 10:47:15 +0530

    Adding WebSite Roles - Completed

---
+++ Hospital/Hospital.Utilities/DbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using Hospital.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Utilities
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private ApplicationDbContext _context;
+
+        public DbInitializer(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+        public void Initialize()
+        {
+           try
+           {
+                if(_context.Database.GetPendingMigrations().Count()>0)
+                {
+
+                }
+           }
+           catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
